aa80ac59a390f9551526255946b31ae8f2f9d85a
manikantaDev2510 <balu25102002@gmail.com>
2025-07-02 11:55:25 +0530

    Server

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ import cartRouter from './routes/cart.route.js'
 import addressRouter from './routes/address.route.js'
 import orderRouter from './routes/order.route.js'
 
+
 const app = express()
 app.use(cors({
     credentials: true,
